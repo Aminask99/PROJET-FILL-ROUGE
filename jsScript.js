beff8e6bdcf194579a1c86fd560c0f193b1c82cb
amina <aminasalik012@gmail.com>
2022-05-30 11:05:52 +0100

    commit a projet fill Rouge

---
+++ jsScript.js
@@ -0,0 +1,13 @@
+
+function modells()
+{
+        document.getElementById("model_parent").style.display = "flex";
+}
+function close_()
+
+{
+        document.getElementById("model_parent").style.display = "none";
+     
+}
+
+
